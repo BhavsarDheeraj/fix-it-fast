8d65a4ade9a17690ae6e0f904e7e08f5ce81b60d
0xdhee <dheeraj@questt.com>
2024-07-24 22:01:54 +0530

    style the active link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,4 +1,8 @@
+"use client";
+
+import classNames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { AiTwotoneBug } from "react-icons/ai";
 
 const NavBar = () => {
@@ -6,6 +10,8 @@ const NavBar = () => {
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
+
+  const currentPath = usePathname();
 
   return (
     <nav className="flex space-x-6 border-b px-6 mb-5 h-14 items-center">
@@ -16,7 +22,11 @@ const NavBar = () => {
         {links.map((link) => (
           <li key={link.href}>
             <Link
-              className="text-zinc-500 hover:text-zinc-900 text-lg transition-colors"
+              className={classNames({
+                "text-zinc-900": currentPath === link.href,
+                "text-zinc-500": currentPath !== link.href,
+                "hover:text-zinc-800 text-lg transition-colors": true,
+              })}
               href={link.href}
             >
               {link.label}
