776c936de7572162a9036ab2773feb3a6f6c33c4
0xdhee <dheeraj@questt.com>
2024-07-26 16:12:15 +0530

    Add: Issues Page

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -2,6 +2,11 @@ import { createIssueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
+export async function GET(request: NextRequest) {
+  const issues = await prisma.issue.findMany();
+  return NextResponse.json(issues);
+}
+
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = createIssueSchema.safeParse(body);
