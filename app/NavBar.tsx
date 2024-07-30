eec4f067409191b0858d054f14ef3ff1753adde6
BhavsarDheeraj <dheerajbhavsar940@gmail.com>
2024-07-30 17:04:02 +0530

    fix: navbar ui

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Box, Spinner } from "@radix-ui/themes";
+import { Box, Container, Flex, Spinner } from "@radix-ui/themes";
 import classNames from "classnames";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
@@ -17,35 +17,43 @@ const NavBar = () => {
   const { status, data: session } = useSession();
 
   return (
-    <nav className="flex space-x-6 border-b px-6 h-14 items-center">
-      <Link href="/">
-        <AiTwotoneBug size={24} />
-      </Link>
-      <ul className="flex space-x-6">
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link
-              className={classNames({
-                "text-zinc-900": currentPath === link.href,
-                "text-zinc-500": currentPath !== link.href,
-                "hover:text-zinc-800 text-lg transition-colors": true,
-              })}
-              href={link.href}
-            >
-              {link.label}
+    <nav className="border-b px-6">
+      <Container>
+        <Flex py="4" align="center" justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <AiTwotoneBug size={24} />
             </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {status === "authenticated" && (
-          <Link href="/api/auth/signout">Logout</Link>
-        )}
-        {status === "unauthenticated" && (
-          <Link href="/api/auth/signin">Login</Link>
-        )}
-        {status === "loading" && <Spinner loading />}
-      </Box>
+            <ul className="flex space-x-6">
+              {links.map((link) => (
+                <li key={link.href}>
+                  <Link
+                    className={classNames({
+                      "text-zinc-900": currentPath === link.href,
+                      "text-zinc-500": currentPath !== link.href,
+                      "hover:text-zinc-800 text-lg transition-colors": true,
+                    })}
+                    href={link.href}
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Flex>
+            <Box>
+              {status === "authenticated" && (
+                <Link href="/api/auth/signout">Logout</Link>
+              )}
+              {status === "unauthenticated" && (
+                <Link href="/api/auth/signin">Login</Link>
+              )}
+              {status === "loading" && <Spinner loading />}
+            </Box>
+          </Flex>
+        </Flex>
+      </Container>
     </nav>
   );
 };
