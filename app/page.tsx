e54b2d59a275e5b78847ed3467b0dc410527602a
BhavsarDheeraj <dheerajbhavsar940@gmail.com>
2024-08-01 15:04:59 +0530

    add: pagination ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <div className="">Hello</div>;
+import Pagination from "./components/Pagination";
+
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
+  return (
+    <Pagination
+      currentPage={parseInt(searchParams.page)}
+      itemCount={100}
+      pageSize={10}
+    />
+  );
 }
